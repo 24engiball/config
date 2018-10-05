295d95f59607204c815b549dd1c6ef18193018fd
Hades <hades@Hadess-MacBook-Pro.local>
2018-10-05 09:47:43 +0700

    add waitmysql

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 sudo docker-compose down
 sudo docker-compose up -d
-sleep 30
-sudo docker-compose exec backend php artisan key:generate
-sudo docker-compose exec backend php artisan migrate:refresh --seed
-sudo docker-compose exec backend php artisan migrate --seed
