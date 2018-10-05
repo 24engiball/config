1507e6bbeccc059a020fd2124671af8c29f97466
Hades <hades@Hadess-MacBook-Pro.local>
2018-10-05 09:32:16 +0700

    edit

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo docker-compose down
 sudo docker-compose up -d
-sleep(30)
+sleep 30
 sudo docker-compose exec backend php artisan key:generate
 sudo docker-compose exec backend php artisan migrate:refresh --seed
 sudo docker-compose exec backend php artisan migrate --seed
