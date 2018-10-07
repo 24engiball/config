4afc22170093c03b2758cc98edc920be6eac5d20
24engiball <engiball@gmail.com>
2018-10-07 21:35:15 +0700

    commit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 sudo docker-compose down
-#s\\udo docker rmi taskapi:0.99
-sudo docker volume prune -f
-sudo docker rmi $(sudo docker images -q)
+sudo docker rmi taskapi:0.0.2
+sudo docker rmi taskapp:0.0.2
 sudo docker-compose rm
 sudo docker-compose up
 
