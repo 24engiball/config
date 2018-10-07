30b79897786ff5a7ae6921ccad648c82666a3c6d
24engiball <engiball@gmail.com>
2018-10-08 02:41:30 +0700

    commit

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 sudo docker-compose down
 sudo docker rmi taskapi:0.0.2
 sudo docker rmi taskapp:0.0.2
+sudo docker volume prune -f
+sudo docker  system prune -a -f
 sudo docker-compose rm
 sudo docker-compose up -d
+sudo docker-compose exec backend ./test.sh
