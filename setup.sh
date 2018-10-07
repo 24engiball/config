bd61668f2ce6a5cbb8de04f15282317e780bffe2
24engiball <engiball@gmail.com>
2018-10-07 18:49:18 +0700

    commit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo docker-compose down
-#sudo docker rmi taskapi:0.99
+#s\\udo docker rmi taskapi:0.99
 sudo docker volume prune -f
+sudo docker rmi $(sudo docker images -q)
 sudo docker-compose rm
 sudo docker-compose up
 
